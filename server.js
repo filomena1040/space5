3bc37b5e1ae97345c3538694a4248f9e766b2215
filomena1040 <120242762+filomena1040@users.noreply.github.com>
2024-01-08 04:48:49 -0800

    1

--- server.js
+++ server.js
@@ -11,45 +11,49 @@ app.use(bodyParser.raw());
 app.set("port", process.env.PORT);
 
 app.post("*", function (req, res) { 
-  req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
-  var head = req.headers;
-  var headerss = JSON.stringify(head)
-    .replace("'{", "")
-    .replace("}'", "")
-    .replace("127.0.0.1:4444", "localhost");
-  var urrrl = "http://185.20.184.142" + req.url;
-  requester.post(
-    {
-      headers: head,
-      url: urrrl,
-      form: req.body,
-      json: false,
-    }
-  ).pipe(res);
+  try{
+      req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
+      var head = req.headers;
+      var headerss = JSON.stringify(head)
+        .replace("'{", "")
+        .replace("}'", "")
+        .replace("127.0.0.1:4444", "localhost");
+      var urrrl = "http://185.20.184.142" + req.url;
+      requester.post(
+        {
+          headers: head,
+          url: urrrl,
+          form: req.body,
+          json: false,
+        }
+      ).pipe(res);
+  }catch{}
 });
 
 app.get("*", function (req, res) {
-  req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
-  var head = req.headers;
-  var headerss = JSON.stringify(head)
-    .replace("'{", "")
-    .replace("}'", "")
-    .replace("127.0.0.1:4444", "localhost:2057");
-  var querystring = req.url;
-  var urrrl = "http://185.20.184.142" + req.url;
-
-  if (!querystring.includes("assets") || !querystring.includes(".")) {
-    requester.post(
-      {
-        headers: head,
-        url: urrrl,
-        body: req.body,
-        json: true,
+  try{
+      req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
+      var head = req.headers;
+      var headerss = JSON.stringify(head)
+        .replace("'{", "")
+        .replace("}'", "")
+        .replace("127.0.0.1:4444", "localhost:2057");
+      var querystring = req.url;
+      var urrrl = "http://185.20.184.142" + req.url;
+    
+      if (!querystring.includes("assets") || !querystring.includes(".")) {
+        requester.post(
+          {
+            headers: head,
+            url: urrrl,
+            body: req.body,
+            json: true,
+          }
+        ).pipe(res);
+      } else {
+        requester.get(urrrl).pipe(res);
       }
-    ).pipe(res);
-  } else {
-    requester.get(urrrl).pipe(res);
-  }
+  }catch{}
 });
 
 app.listen(process.env.PORT, "0.0.0.0", function () {
